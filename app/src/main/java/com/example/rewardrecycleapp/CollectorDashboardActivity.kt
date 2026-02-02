92b2dda618b519fa04cfc3d1a0bd1bc4486fa631
karththiga <karththiga11@gmail.com>
2026-02-02 08:10:33 +0530

    Move collector summaries to history tab

---
+++ app/src/main/java/com/example/rewardrecycleapp/CollectorDashboardActivity.kt
@@ -0,0 +1,38 @@
+package com.example.rewardrecycleapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.rewardrecycleapp.databinding.ActivityCollectorDashboardBinding
+
+class CollectorDashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCollectorDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCollectorDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            loadFragment(CollectorDashboardFragment())
+        }
+
+        binding.collectorBottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> loadFragment(CollectorDashboardFragment())
+                R.id.nav_history -> loadFragment(CollectorHistoryFragment())
+                R.id.nav_notification -> loadFragment(NotificationFragment())
+                R.id.nav_profile -> loadFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.collectorDashboardContainer, fragment)
+            .commit()
+    }
+}
