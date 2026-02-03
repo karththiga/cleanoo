ef070b5f57635b7fec6ca8d7b2b32dbbb10c8275
karththiga <karththiga11@gmail.com>
2026-02-03 07:17:28 +0530

    Refine collector job details and remove completed button

---
+++ app/src/main/java/com/example/rewardrecycleapp/CollectorJobDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.example.rewardrecycleapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class CollectorJobDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_collector_job_detail, container, false)
+    }
+}
